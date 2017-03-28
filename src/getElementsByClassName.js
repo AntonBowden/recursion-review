c2d80d20e759364ed6cfcc27c9487800d2c49f91
Anton Bowden <bowden.anton@gmail.com>
2017-03-28 16:38:28 +0100

    Complete bare minimum

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+var getElementsByClassName = function(className) {
   // your code here
+  const elements = [];
+
+  function scan(node) {
+    if(node.classList && Array.from(node.classList).includes(className)) {
+      elements.push(node);
+    }
+
+    for(let i = 0; i < node.childNodes.length; i++) {
+      scan(node.childNodes[i]);
+    }
+  }
+
+  scan(document.body);
+
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  let dummy = '';
+  let item;
+
+  if(Array.isArray(obj)) {
+    for(let i = 0; i < obj.length; i++) {
+      item = stringifyJSON(obj[i]);
+      dummy += (item === 'undefined' ? 'null,' : `${item},`);
+    }
+
+    return `[${dummy.slice(0, -1)}]`;
+  } else if(typeof obj === 'object' && obj !== null) {
+    for(let key in obj) {
+      item = stringifyJSON(obj[key]);
+
+      if(item !== 'undefined') {
+        dummy += `"${key}":${item},`;
+      }
+    }
+
+    return `{${dummy.slice(0, -1)}}`;
+  } else if(typeof obj === 'string') {
+    return `"${obj}"`;
+  } else if(typeof obj === 'boolean') {
+    return obj ? 'true' : 'false';
+  } else if(obj === undefined || typeof obj === 'function') {
+    return 'undefined';
+  } else if(obj === null || isNaN(obj) || obj === Infinity || obj === -Infinity) {
+    return 'null';
+  } else if(typeof obj === 'number') {
+    return `${obj}`;
+  }
 };
